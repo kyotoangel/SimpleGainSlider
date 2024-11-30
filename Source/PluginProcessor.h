3d9c1ab1561626a92ce00488c0aa6f2aca2d82b4
Nokami <antonin.roux38@gmail.com>
2024-11-30 11:39:29 +0100

    deuxieme commit

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,7 +53,7 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    double RawVolume;
+    float RawVolume = 0.0f;
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleGainSliderAudioProcessor)
